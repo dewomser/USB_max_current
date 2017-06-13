85be7b781b38937e6cd97cfe6f177202df64db49
Stefan Höhn <webmaster@untergang.de>
2017-06-13 12:49:36 +0200

    Add files via upload
    Extra spaces for better Design

--- usb_all_current.sh
+++ usb_all_current.sh
@@ -1,6 +1,7 @@
+ 
 #!/bin/bash
 echo ""
-echo -e "\e[1;4m ## Maximal current allowed from all USB "
+echo -e "\e[1;4m ## Maximal current allowed from all USB Devices"
 zaehl=$(lsusb -s 001|wc -l)
 for (( i=1; i <= $zaehl; i++ ))
 do
@@ -11,7 +12,7 @@ echo "---------------------------"
 wert=$(lsusb -v 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//') 
 echo "Bus ∑ $(dc <<< '[+]sa[z2!>az2!>b]sb'"${wert[*]}lbxp") mA"
 echo ""
-echo -e "\e[1;4m ## All devices, maximal current"
-wert=$(lsusb -v  2>&1 | grep  '\(id[PV]\|MaxPower\)' | sed 's/idProduct// ; s/idVendor//'|sed 's/^[ \t]*// ; s/0x[A-Za-z0-9]*// '|sed 's/^[[:blank:]]*$/not defined/' )
+echo -e "\e[1;4m ## All USB devices, maximal current"
+wert=$(lsusb -v  2>&1 | grep  '\(id[PV]\|MaxPower\)' | sed 's/idProduct// ; s/idVendor//'|sed 's/^[ \t]*// ; s/0x[A-Za-z0-9]*//'| sed 's/mA/mA\\n/'|sed 's/^[[:blank:]]*$/ not defined/' )
 echo -e "\e[0m $wert"
 echo ""
