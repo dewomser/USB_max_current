847f3d8052f6f5de3a38422066bad0c9f18403eb
Stefan Höhn <webmaster@untergang.de>
2021-03-14 03:22:45 +0100

    shellcheck verbessert

--- usb_max_current-bus.sh
+++ usb_max_current-bus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 zaehl=$(lsusb -s 001|wc -l)
-for (( i=1; i <= $zaehl; i++ ))
+for (( i=1; i <= zaehl; i++ ))
 do
-wert=$(lsusb -v -s $i: 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//') 
-echo Bus $i $(dc <<< '[+]sa[z2!>az2!>b]sb'"${wert[*]}lbxp") mA 
+wert=$(lsusb -v -s "$i": 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//') 
+echo Bus "$i" "$(dc <<< '[+]sa[z2!>az2!>b]sb'"${wert[*]}lbxp")" mA 
 done
