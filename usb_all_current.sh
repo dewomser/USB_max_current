54f23f326d50ec851acd92eecfe798ca6c2e0c88
Stefan Höhn <webmaster@untergang.de>
2021-03-14 03:17:42 +0100

    shellcheck optimiert

--- usb_all_current.sh
+++ usb_all_current.sh
@@ -1,11 +1,10 @@
- 
 #!/bin/bash
 echo ""
 echo -e "\e[1;4m ## Maximal current allowed from all USB Devices"
 zaehl=$(lsusb -s 001|wc -l)
-for (( i=1; i <= $zaehl; i++ ))
+for (( i=1; i <= zaehl; i++ ))
 do
-wert=$(lsusb -v -s $i: 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//') 
+wert=$(lsusb -v -s "$i": 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//') 
 echo -e "\e[0m Bus $i $(dc <<< '[+]sa[z2!>az2!>b]sb'"${wert[*]}lbxp") mA" 
 done
 echo "---------------------------"
