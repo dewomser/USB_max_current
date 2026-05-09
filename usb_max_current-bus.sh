c3b74d5850fa4fae06a2bf93e3fdbc68a7522bcd
Stefan Höhn <webmaster@untergang.de>
2026-05-09 09:04:32 +0200

    Fehler im Code durch Copilot entfernen lassen
    Refactor script to calculate total MaxPower for USB devices.

--- usb_max_current-bus.sh
+++ usb_max_current-bus.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 zaehl=$(lsusb -s 001|wc -l)
+#echo $zaehl
 for (( i=1; i <= zaehl; i++ ))
 do
-wert=$(lsusb -v -s "$i": 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//') 
-echo Bus "$i" "$(dc <<< '[+]sa[z2!>az2!>b]sb'"${wert[*]}lbxp")" mA 
+  wert=$(lsusb -v -s $i: 2>&1 | grep MaxPower | sed 's/MaxPower//;s/mA//' | tr -d ' ')
+  total=$(echo "$wert" | awk '{sum+=$1} END {print sum}')
+  echo "Bus $i $total mA"
 done
